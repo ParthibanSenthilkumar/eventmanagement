94b1556dd17f589ce2de8d1a04a66ee27ff3fbeb
Parthiban <parthiban.dev06@gmail.com>
2026-02-25 18:43:53 +0530

    card style added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import Container from "react-bootstrap/esm/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Home = () => {
+  const [EventList, setEventList] = useState([]);
+
   return (
     <>
       <Header />
+      <Container>
+        <Row>
+          <Col lg={4}>
+            {EventList.map((eventsdata) => {
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>;
+            })}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
